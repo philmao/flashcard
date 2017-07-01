ee3b5782c026cf2ca6261e0bd06a53769ea1040e
philmao <philip_mao@yahoo.com>
2017-07-01 09:05:40 -0700

    add scope-safe constructor

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,12 +1,17 @@
 
 var ClozeCard = function(fullText, cloze) {
 	if(fullText.indexOf(cloze) < 0) {
-		console.log("Error: cloze not part of full text!");
+		console.log("\nError: cloze not part of full text!\n");
 	}
 	else {
-		this.fullText = fullText;
-		this.partial = fullText.replace(cloze, "...");
-		this.cloze = cloze;
+		if(this instanceof ClozeCard) {
+			this.fullText = fullText;
+			this.partial = fullText.replace(cloze, "...");
+			this.cloze = cloze;
+		} else {
+			return new ClozeCard(fullText, cloze);
+		}
+
 	}
 }
 module.exports = ClozeCard;
--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var BasicCard = require("./BasicCard.js");
 var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
 
+console.log("BasicCard:")
 // "Who was the first president of the United States?"
 console.log(firstPresident.front); 
 
@@ -15,6 +16,7 @@ console.log(firstPresident.back);
 var firstPresidentCloze = new ClozeCard(
     "George Washington was the first president of the United States.", "George Washington");
 
+console.log("\nClozeCard:")
 // "George Washington"
 console.log(firstPresidentCloze.cloze); 
 
@@ -28,7 +30,7 @@ console.log(firstPresidentCloze.fullText);
 var brokenCloze = new ClozeCard("This doesn't work", "oops");
 
 BasicCard.prototype.printFront = function printFront() {
-	console.log(this.front);
+	console.log("\nBasicCard: " + this.front);
 }
 BasicCard.prototype.printBack = function printBack() {
 	console.log("\nThe answer is: ", this.back);
@@ -36,15 +38,27 @@ BasicCard.prototype.printBack = function printBack() {
 
 
 ClozeCard.prototype.printFront = function printFront() {
-	console.log("Please fill in the blanks: \n");
+	console.log("\nClozeCard: please fill in the blanks: \n");
 	console.log(this.partial);
 }
 ClozeCard.prototype.printBack = function printBack() {
 	console.log("\nThe answer is: ", this.cloze);
 	console.log(this.fullText);
 }
+
 firstPresident.printFront();
 firstPresident.printBack();
 
 firstPresidentCloze.printFront();
 firstPresidentCloze.printBack();
+
+var thirdPresident = BasicCard(
+	"Who was the third president of the United States?", "Thomas Jefferson");
+var thirdPresidentCloze = ClozeCard(
+	"Thomas Jefferson was the first president of the United States.", "Thomas Jefferson");
+
+thirdPresident.printFront();
+thirdPresident.printBack();
+
+thirdPresidentCloze.printFront();
+thirdPresidentCloze.printBack();
